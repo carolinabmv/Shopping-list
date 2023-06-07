968f107c8f5f8070c7b6c7aa9ed5df1173d6ad9e
carolinabmv <anacarolinabmv@gmail.com>
2023-06-07 12:58:54 +0200

    Refactoring

--- script.js
+++ script.js
@@ -24,44 +24,34 @@ closeBtns.forEach((btn) =>
   })
 );
 
-function newList() {
-  itemsList = document.querySelectorAll('.check');
-  closeBtns = document.querySelectorAll('.close-btn');
-  const lastItemPosition = itemsList.length - 1;
-  const lastItem = itemsList[lastItemPosition];
-  const lastCloseBtn = closeBtns[lastItemPosition];
-  lastItem.addEventListener('click', function () {
-    strikeOut(this);
-  });
-  lastCloseBtn.addEventListener('click', function () {
-    deleteItem(this);
-  });
-}
-
 const addNewItem = () => {
   let item = enterInputEl.value;
-
   item = item.replace(item[0], item[0].toUpperCase());
 
   const checkInput = document.createElement('input');
   checkInput.setAttribute('type', 'checkbox');
   checkInput.setAttribute('class', 'check');
+  checkInput.addEventListener('click', function () {
+    strikeOut(this);
+  });
 
   const closeIcon = document.createElement('i');
   closeIcon.setAttribute('class', 'fa-solid fa-xmark fa-xs');
   const close = document.createElement('a');
   close.setAttribute('href', '#');
   close.setAttribute('class', 'close-btn');
-  close.appendChild(closeIcon);
+  close.append(closeIcon);
+  close.addEventListener('click', function () {
+    deleteItem(this);
+  });
 
   const li = document.createElement('li');
-  li.appendChild(checkInput);
-  li.appendChild(document.createTextNode(` ${item}`));
-  li.appendChild(close);
-  ul.appendChild(li);
+  li.append(checkInput);
+  li.append(document.createTextNode(` ${item}`));
+  li.append(close);
+  ul.append(li);
 
   enterInputEl.value = '';
-  newList();
 };
 
 enterBtn.addEventListener('click', () => {
